8786a21429cf92d04f7f6ff4aa5b73b7b4d879c6
kerlonfernandes <kerlo1221@gmail.com>
2024-06-26 11:08:49 -0300

    structure

---
+++ phproj/phproj/main.py
@@ -0,0 +1,16 @@
+import argparse
+
+def create_project(name):
+    print(f"project '{name}' created successfuly!")
+
+def main():
+    parser = argparse.ArgumentParser(description='PHP project manager')
+    parser.add_argument('--create-project', type=str, help='Create a new project ex: phproj --create-project project-name')
+    args = parser.parse_args()
+
+    if args.create_project:
+        create_project(args.create_project)
+
+
+if __name__ == '__main__':
+    main()
